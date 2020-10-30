8aacf3fe416e2b9e1116e32b76048ff49916c0cb
santSykes <70683976+santSykes@users.noreply.github.com>
2020-10-30 11:15:13 -0600

    smarter ghosts modificados

--- pacman.py
+++ pacman.py
@@ -1,160 +1,160 @@
-"""Pacman, classic arcade game.
-
-Exercises
-
-1. Change the board.
-2. Change the number of ghosts.
-3. Change where pacman starts.
-4. Make the ghosts faster/slower.
-5. Make the ghosts smarter.
-
-"""
-
 from random import choice
 from turtle import *
 from freegames import floor, vector
 
+#Función para construir el tablero
 state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+[vector(-180, 160), vector(5, 0)],
+[vector(-180, -160), vector(0, 5)],
+[vector(100, 160), vector(0, -5)],
+[vector(100, -160), vector(-5, 0)],
 ]
+
+#TABLERO: El tablero funciona de la siguiente manera, esta constituido por una matrix de ceros y unos.
+#Los ceros representan espacios vacios t los uno representan parte del tablero por el que puede pasar el pac-man
+#Para cabiar el tablero, solo basta con cambiar los dígitos de la matriz, intercalando entre ceros y unos.
 tiles = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0,
+0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0,
+0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
+0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0,
+0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0,
+0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0,
+0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 
+
+#Funcion que crea la forma del tablero usando cuadrados
 def square(x, y):
-    "Draw square using path at (x, y)."
-    path.up()
-    path.goto(x, y)
-    path.down()
-    path.begin_fill()
+"Draw square using path at (x, y)."
+path.up()
+path.goto(x, y)
+path.down()
+path.begin_fill()
 
-    for count in range(4):
-        path.forward(20)
-        path.left(90)
+for count in range(4):
+path.forward(20)
+path.left(90)
 
-    path.end_fill()
+path.end_fill()
 
 def offset(point):
-    "Return offset of point in tiles."
-    x = (floor(point.x, 20) + 200) / 20
-    y = (180 - floor(point.y, 20)) / 20
-    index = int(x + y * 20)
-    return index
+"Return offset of point in tiles."
+x = (floor(point.x, 20) + 200) / 20
+y = (180 - floor(point.y, 20)) / 20
+index = int(x + y * 20)
+return index
+
+#Funcion que retorna un valor booleano True si la posición cuando pac-man se mueve a una posición valida
 
 def valid(point):
-    "Return True if point is valid in tiles."
-    index = offset(point)
+"Return True if point is valid in tiles."
+index = offset(point)
 
-    if tiles[index] == 0:
-        return False
+if tiles[index] == 0:
+return False
 
-    index = offset(point + 19)
+index = offset(point + 19)
 
-    if tiles[index] == 0:
-        return False
+if tiles[index] == 0:
+return False
 
-    return point.x % 20 == 0 or point.y % 20 == 0
+return point.x % 20 == 0 or point.y % 20 == 0
 
+#Funcion que cambia los colores del tablero y crea la comida de pac-man
 def world():
-    "Draw world using path."
-    bgcolor('black')
-    path.color('blue')
+"Draw world using path."
+bgcolor('black')
+path.color('blue')
 
-    for index in range(len(tiles)):
-        tile = tiles[index]
+for index in range(len(tiles)):
+tile = tiles[index]
 
-        if tile > 0:
-            x = (index % 20) * 20 - 200
-            y = 180 - (index // 20) * 20
-            square(x, y)
+if tile > 0:
+x = (index % 20) * 20 - 200
+y = 180 - (index // 20) * 20
+square(x, y)
 
-            if tile == 1:
-                path.up()
-                path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
-
+if tile == 1:
+path.up()
+path.goto(x + 10, y + 10)
+path.dot(2, 'white')
+#Función que mueve los fantasmas y pac-man, este se encarga de mover los fantasmas mediante vectores que van en los ejes x y y.
+#También mueve el pac-man de acuerdo a la posición aim, esta cambia de acuerdo a la posición del pac-man.
 def move():
-    "Move pacman and all ghosts."
-    writer.undo()
-    writer.write(state['score'])
+"Move pacman and all ghosts."
+writer.undo()
+writer.write(state['score'])
 
-    clear()
+clear()
 
-    if valid(pacman + aim):
-        pacman.move(aim)
+if valid(pacman + aim):
+pacman.move(aim)
 
-    index = offset(pacman)
+index = offset(pacman)
 
-    if tiles[index] == 1:
-        tiles[index] = 2
-        state['score'] += 1
-        x = (index % 20) * 20 - 200
-        y = 180 - (index // 20) * 20
-        square(x, y)
+if tiles[index] == 1:
+tiles[index] = 2
+state['score'] += 1
+x = (index % 20) * 20 - 200
+y = 180 - (index // 20) * 20
+square(x, y)
 
-    up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
+up()
+goto(pacman.x + 10, pacman.y + 10)
+dot(20, 'yellow')
 
-    for point, course in ghosts:
-        if valid(point + course):
-            point.move(course)
-        else:
-            options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
-            ]
-            plan = choice(options)
-            course.x = plan.x
-            course.y = plan.y
+for point, course in ghosts:
+if valid(point + course):
+point.move(course)
+else:
+options = [
+vector(15, 0),
+vector(-15, 0),
+vector(0, 15),
+vector(0,-15),
+]
+plan = choice(options)
+course.x = plan.x
+course.y = plan.y
 
-        up()
-        goto(point.x + 10, point.y + 10)
-        dot(20, 'red')
+up()
+goto(point.x + 15, point.y + 15)
+dot(20, 'red')
 
-    update()
+update()
 
-    for point, course in ghosts:
-        if abs(pacman - point) < 20:
-            return
+for point, course in ghosts:
+if abs(pacman - point) < 20:
+return
 
-    ontimer(move, 100)
-
+ontimer(move, 100)
+#Función que cambia la posición del pacman cada vez que se mueve.
 def change(x, y):
-    "Change pacman aim if valid."
-    if valid(pacman + vector(x, y)):
-        aim.x = x
-        aim.y = y
+"Change pacman aim if valid."
+if valid(pacman + vector(x, y)):
+aim.x = x
+aim.y = y
 
+#Set de color y puntuación
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
@@ -162,6 +162,7 @@ writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
 listen()
+#Set para mover el pac-man
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
